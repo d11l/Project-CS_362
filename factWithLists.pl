6aa21af78b667905f5b38f08de73ad9efb5ac3da
Abdulrhman radhi <abdulrahman.radhi.2002@gmail.com>
2023-10-25 15:06:07 +0300

    Init Project

---
+++ factWithLists.pl
@@ -0,0 +1,39 @@
+animal_facts([
+    animal(lion),
+    animal(tiger),
+    animal(elephant),
+    animal(zebra),
+    animal(dolphin),
+    animal(penguin),
+    animal(gorila)
+]).
+
+color_facts([
+    color(lion, tan),
+    color(tiger, orange_and_black),
+    color(elephant, gray),
+    color(zebra, black_and_white),
+    color(dolphin, gray),
+    color(penguin, black_and_white),
+    color(gorila, black_and_brown)
+]).
+
+diet_facts([
+    diet(lion, carnivore),
+    diet(tiger, carnivore),
+    diet(elephant, herbivore),
+    diet(zebra, herbivore),
+    diet(dolphin, carnivore),
+    diet(penguin, carnivore),
+    diet(gorila, herbivore)
+]).
+
+species_facts([
+    species(lion, mammals),
+    species(tiger, mammals),
+    species(elephant, mammals),
+    species(zebra, mammals),
+    species(dolphin, mammals),
+    species(penguin, birds),
+    species(gorila, mammals)
+]).
