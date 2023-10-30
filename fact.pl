11640b56478583e8137665f7503a1f6b0027ffa2
Abdulrhman radhi <abdulrahman.radhi.2002@gmail.com>
2023-10-30 19:42:13 +0300

    add more fact

--- fact.pl
+++ fact.pl
@@ -5,7 +5,36 @@ animal(zebra).
 animal(dolphin).
 animal(penguin).
 animal(gorila).
+animal(wolf).
+animal(fox).
+animal(horse).
+animal(deer).
+animal(turtle).
+animal(crocodile).
+animal(snake).
 
+place_live(lion,terrestrial).
+place_live(tiger,terrestrial).
+place_live(elephant,terrestrial).
+place_live(zebra,terrestrial).
+place_live(snake,terrestrial).
+place_live(penguin,terrestrial).
+place_live(gorilla,terrestrial).
+place_live(wolf,terrestrial).
+place_live(fox,terrestrial).
+place_live(horse,terrestrial).
+place_live(deer,terrestrial).
+place_live(crocodile,terrestrial).
+place_live(dolphin,aquatic).
+place_live(turtle,aquatic).
+
+color(wolf,grey_and_black). 
+color(fox,orange_and_white). 
+color(horse,brown). 
+color(deer,golden). 
+color(turtle,green_and_brown). 
+color(crocodile,green). 
+color(snake,black_and_white).
 color(lion,tan).
 color(tiger,orang_and_black).
 color(elephant,gray).
@@ -16,10 +45,17 @@ color(gorila,back_and_brown).
 
 dite(lion,carnivore).
 dite(tiger,carnivore).
-dite(elephant,herbivore).
-dite(zebra,herbivore).
 dite(dolphin,carnivore).
 dite(penguin,carnivore).
+dite(wolf,carnivore). 
+dite(fox,carnivore). 
+dite(crocodile,carnivore). 
+dite(snake,carnivore).
+dite(zebra,herbivore).
+dite(elephant,herbivore).
+dite(horse,herbivore). 
+dite(deer,herbivore). 
+dite(turtle,herbivore).
 dite(gorila,herbivore).
 
 species(lion,mammals).
@@ -29,7 +65,18 @@ species(zebra,mammals).
 species(dolphin,mammals).
 species(penguin,birds).
 species(gorila,mammals).
+species(wolf,mammals). 
+species(fox,mammals). 
+species(horse,mammals). 
+species(deer,mammals). 
+species(turtle,reptile). 
+species(crocodile,reptile). 
+species(snake,reptile).
 
+is_carnivore(X):-animal(X),dite(X,carnivore).
+is_herbivore(X):-animal(X),dite(X,herbivore).
+same_species(X,Y):-animal(X),animal(Y),species(X,Z),species(Y,Z).
+same_dite(X,Y):-animal(X),animal(Y),dite(X,Z),dite(Y,Z).
 
 animalInfo(Animal, Color, Diet, Species) :- 
     animal(Animal), 
